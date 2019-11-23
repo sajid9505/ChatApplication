2ddb51d43af7aee68478873dab2b3cb7c3da8751
sajid9505 <sajid.mahmud9505@gmail.com>
2019-11-23 22:10:41 +0600

    emitting events

--- index.js
+++ index.js
@@ -10,10 +10,16 @@ app.get('/', function(req, res){
 //   console.log('a user connected');
 // });
 
+// io.on('connection', function(socket){
+//   console.log('a user connected');
+//   socket.on('disconnect', function(){
+//     console.log('user disconnected');
+//   });
+// });
+
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
   });
 });
 
